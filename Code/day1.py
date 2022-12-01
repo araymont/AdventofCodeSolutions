c89fea08ec953ea1335843fca0b3d155fb452241
lyx3 <Burningflame201@gmail.com>
2022-12-01 11:53:30 +0000

    Part1 complete
    Done part one!

--- Code/day1.py
+++ Code/day1.py
@@ -1,9 +1,19 @@
-
+sum=0
+max1=0
 f = open("day1Input.txt", 'r')
 
 with open('day1Input.txt') as f:
-    lines = [line.rstrip() for line in f]
-print(lines)
+    for line in f:
+        lines=line.rstrip()
+        if lines == '':
+            if sum>=max1:
+                max1=sum
+            sum=0
+        else:
+            sum=sum+int(lines)
+
+
+print(max1)
 
 f.close()
 
