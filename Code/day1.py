a972dff78b6ef12d876df23b7d5db4b42f821326
lyx3 <Burningflame201@gmail.com>
2022-12-01 12:04:42 +0000

    Completed day1 Fully
    Fully finished AoC day 1!

--- Code/day1.py
+++ Code/day1.py
@@ -1,5 +1,7 @@
 sum=0
 max1=0
+max2=0
+max3=0
 f = open("day1Input.txt", 'r')
 
 with open('day1Input.txt') as f:
@@ -7,13 +9,22 @@ with open('day1Input.txt') as f:
         lines=line.rstrip()
         if lines == '':
             if sum>=max1:
+                max3=max2
+                max2=max1
                 max1=sum
+            elif sum>=max2:
+                max3=max2
+                max2=sum
+            elif sum>=max3:
+                max3=sum
             sum=0
         else:
             sum=sum+int(lines)
 
-
-print(max1)
+temp=max1+max2+max3
+print(temp)
+print(max1+max2+max3)
+print(max1,max2,max3)
 
 f.close()
 
